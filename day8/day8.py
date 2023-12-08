7c1e35e4ef8dd43c1731966fa56d31185433219f
Gonzalo <golmofernandez@gmail.com>
2023-12-08 11:34:17 +0100

    day8 part2

--- day8/day8.py
+++ day8/day8.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def parse_node(line: str):
     line = line.replace(" ", "")
     name, options = line.split("=")
@@ -31,4 +34,46 @@ def part1():
     return count
 
 
+def mcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def mcm(a, b):
+    return (a * b) // mcd(a, b)
+
+
+def part2():
+    file = "input.txt"
+    instructions, nodes = parse_input(file)
+    nodes = [parse_node(node) for node in nodes]
+
+    direction = {"L": 0, "R": 1}
+    # Create a map of the nodes
+    map = {}
+    for node in nodes:
+        map[node[0]] = node[1]
+
+    # Find all nodes ending with 'A'
+    checking_nodes = [node for node in map if node.endswith("A")]
+
+    count = 0
+    end_Z = []
+    top = len(checking_nodes)
+    while len(end_Z) < top:
+        for i in range(len(checking_nodes)):
+            checking_nodes[i] = map[checking_nodes[i]][
+                direction[instructions[count % len(instructions)]]
+            ]
+        count += 1
+        for node in checking_nodes:
+            if node.endswith("Z"):
+                end_Z.append(count)
+                checking_nodes.remove(node)
+
+    return reduce(mcm, end_Z)
+
+
 print("Part 1: ", part1())
+print("Part 2: ", part2())
